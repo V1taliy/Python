e12dac1fb6e8eb63a3fa91e37be9fe5eeeae492e
Vitaliy <vitaliy.sakhno1@gmail.com>
2023-11-23 15:09:17 +0200

    added band Name

---
+++ bandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?")
+print(city)
+petName = input("What's your pet's name?") 
+print(petName)
+print("Your band name could be " + city + " " + petName)
